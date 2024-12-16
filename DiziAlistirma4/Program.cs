eae30344c7f07c1ce4e3a59af6ffdcdbe1a654a3
Halil Özcan <halil_ozcann@outlook.com>
2024-12-16 20:31:21 +0300

    C# Dizi Alistirma 4 Yapıldı.

--- DiziAlistirma4/Program.cs
+++ DiziAlistirma4/Program.cs
@@ -10,6 +10,31 @@ namespace DiziAlistirma4
     {
         static void Main(string[] args)
         {
+            int[] notlar = { 60, 70, 80, 90, 100 };
+
+            int ortalama = 0;
+            int toplam = 0;
+
+            foreach(var item in notlar)
+            {
+                toplam = toplam + item;
+
+                ortalama = toplam / notlar.Length;
+
+                
+            }
+            Console.WriteLine("Not Ortalaması : " + ortalama);
+            Console.WriteLine(" ------------------------------------------------------- ");
+
+            foreach(var item in notlar)
+            {
+                if(item < ortalama)
+                {
+                    Console.WriteLine("Ortalama altında kalan Notlar : " + item);
+                }
+            }
+
+
         }
     }
 }
