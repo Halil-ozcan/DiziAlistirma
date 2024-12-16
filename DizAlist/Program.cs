0d0168e3b595a785bc5184921a063ea0363cc24d
Halil Özcan <halil_ozcann@outlook.com>
2024-12-16 20:04:39 +0300

    C# Dizi Alistirma 1 Yapıldı.

--- DizAlist/Program.cs
+++ DizAlist/Program.cs
@@ -10,6 +10,14 @@ namespace DizAlistirma
     {
         static void Main(string[] args)
         {
+            int[] sayilar = { 1,2,3,4,5,6,7,8,9 };
+            int toplam = 0;
+            foreach (var item in sayilar)
+            {
+                toplam = toplam + item;
+
+                Console.WriteLine("Sonuc : " + toplam);
+            }
         }
     }
 }
