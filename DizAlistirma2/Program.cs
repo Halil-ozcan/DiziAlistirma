babcf9df6b26c0e5379880d06da72260f3e0804c
Halil Özcan <halil_ozcann@outlook.com>
2024-12-16 20:10:19 +0300

    C# Diz Alistirma 2 Yapıldı.

--- DizAlistirma2/Program.cs
+++ DizAlistirma2/Program.cs
@@ -10,6 +10,25 @@ namespace DizAlistirma2
     {
         static void Main(string[] args)
         {
+            int[] sayi = { 5, 12, 3, 7, 19 };
+
+            int enkucuk = sayi[0];
+            int enbuyuk = sayi[0];
+
+            foreach (var item in sayi)
+            {
+                if(item < enkucuk)
+                {
+                    enkucuk = item;
+                }
+                if(item > enbuyuk)
+                {
+                    enbuyuk = item;
+                }
+            }
+
+            Console.WriteLine("Dizideki En Kucuk Sayi : " + enkucuk);
+            Console.WriteLine("Dizideki En Buyuk Sayi : " + enbuyuk);
         }
     }
 }
